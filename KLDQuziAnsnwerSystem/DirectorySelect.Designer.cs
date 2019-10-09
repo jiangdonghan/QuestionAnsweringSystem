63607d4b048f7aaa3e71ada1197b82716c011634
kunal-arora <kunal@happilyunmarried.com>
2019-10-09 17:58:50 +1000

    updated the design for step 1 page

--- KLDQuziAnsnwerSystem/DirectorySelect.Designer.cs
+++ KLDQuziAnsnwerSystem/DirectorySelect.Designer.cs
@@ -35,67 +35,103 @@
             this.IndexDirectoryPath = new System.Windows.Forms.Label();
             this.CollectionDirectoryPath = new System.Windows.Forms.Label();
             this.IndexCreate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonIndexSave
             // 
-            this.buttonIndexSave.ForeColor = System.Drawing.Color.Coral;
-            this.buttonIndexSave.Location = new System.Drawing.Point(195, 61);
+            this.buttonIndexSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.buttonIndexSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonIndexSave.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonIndexSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(204)))), ((int)(((byte)(133)))));
+            this.buttonIndexSave.Location = new System.Drawing.Point(50, 143);
+            this.buttonIndexSave.Margin = new System.Windows.Forms.Padding(2);
             this.buttonIndexSave.Name = "buttonIndexSave";
-            this.buttonIndexSave.Size = new System.Drawing.Size(385, 59);
+            this.buttonIndexSave.Size = new System.Drawing.Size(334, 63);
             this.buttonIndexSave.TabIndex = 3;
-            this.buttonIndexSave.Text = "Select Index Save Directory";
-            this.buttonIndexSave.UseVisualStyleBackColor = true;
+            this.buttonIndexSave.Text = "SELECT LUCENE INDEX DIRECTORY";
+            this.buttonIndexSave.UseVisualStyleBackColor = false;
             this.buttonIndexSave.Click += new System.EventHandler(this.ButtonIndexSave_Click);
             // 
             // buttonCollectionSave
             // 
-            this.buttonCollectionSave.Location = new System.Drawing.Point(195, 222);
+            this.buttonCollectionSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.buttonCollectionSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonCollectionSave.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCollectionSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(204)))), ((int)(((byte)(133)))));
+            this.buttonCollectionSave.Location = new System.Drawing.Point(50, 286);
+            this.buttonCollectionSave.Margin = new System.Windows.Forms.Padding(2);
             this.buttonCollectionSave.Name = "buttonCollectionSave";
-            this.buttonCollectionSave.Size = new System.Drawing.Size(385, 59);
+            this.buttonCollectionSave.Size = new System.Drawing.Size(334, 62);
             this.buttonCollectionSave.TabIndex = 4;
-            this.buttonCollectionSave.Text = "Select Collection Save Directory";
-            this.buttonCollectionSave.UseVisualStyleBackColor = true;
+            this.buttonCollectionSave.Text = "SELECT COLLECTION FILE";
+            this.buttonCollectionSave.UseVisualStyleBackColor = false;
             this.buttonCollectionSave.Click += new System.EventHandler(this.ButtonCollectionSave_Click);
             // 
             // IndexDirectoryPath
             // 
             this.IndexDirectoryPath.AutoSize = true;
-            this.IndexDirectoryPath.Location = new System.Drawing.Point(745, 78);
+            this.IndexDirectoryPath.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IndexDirectoryPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.IndexDirectoryPath.Location = new System.Drawing.Point(48, 111);
+            this.IndexDirectoryPath.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.IndexDirectoryPath.Name = "IndexDirectoryPath";
-            this.IndexDirectoryPath.Size = new System.Drawing.Size(206, 25);
+            this.IndexDirectoryPath.Size = new System.Drawing.Size(121, 15);
             this.IndexDirectoryPath.TabIndex = 5;
             this.IndexDirectoryPath.Text = "Index Directory Path";
             // 
             // CollectionDirectoryPath
             // 
             this.CollectionDirectoryPath.AutoSize = true;
-            this.CollectionDirectoryPath.Location = new System.Drawing.Point(744, 239);
+            this.CollectionDirectoryPath.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CollectionDirectoryPath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.CollectionDirectoryPath.Location = new System.Drawing.Point(47, 257);
+            this.CollectionDirectoryPath.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.CollectionDirectoryPath.Name = "CollectionDirectoryPath";
-            this.CollectionDirectoryPath.Size = new System.Drawing.Size(249, 25);
+            this.CollectionDirectoryPath.Size = new System.Drawing.Size(146, 15);
             this.CollectionDirectoryPath.TabIndex = 6;
             this.CollectionDirectoryPath.Text = "Collection Directory Path";
             // 
             // IndexCreate
             // 
-            this.IndexCreate.Location = new System.Drawing.Point(488, 427);
+            this.IndexCreate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(105)))), ((int)(((byte)(232)))));
+            this.IndexCreate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.IndexCreate.Font = new System.Drawing.Font("Roboto", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IndexCreate.ForeColor = System.Drawing.Color.White;
+            this.IndexCreate.Location = new System.Drawing.Point(51, 425);
+            this.IndexCreate.Margin = new System.Windows.Forms.Padding(2);
             this.IndexCreate.Name = "IndexCreate";
-            this.IndexCreate.Size = new System.Drawing.Size(243, 76);
+            this.IndexCreate.Size = new System.Drawing.Size(184, 56);
             this.IndexCreate.TabIndex = 7;
-            this.IndexCreate.Text = "Create Index";
-            this.IndexCreate.UseVisualStyleBackColor = true;
+            this.IndexCreate.Text = "START INDEXING";
+            this.IndexCreate.UseVisualStyleBackColor = false;
             this.IndexCreate.Click += new System.EventHandler(this.IndexCreate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.label1.Location = new System.Drawing.Point(47, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Step 1: Selecting paths";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
             // 
             // DirectorySelect
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1232, 602);
+            this.ClientSize = new System.Drawing.Size(706, 749);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.IndexCreate);
             this.Controls.Add(this.CollectionDirectoryPath);
             this.Controls.Add(this.IndexDirectoryPath);
             this.Controls.Add(this.buttonCollectionSave);
             this.Controls.Add(this.buttonIndexSave);
+            this.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "DirectorySelect";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -112,6 +148,7 @@
         private System.Windows.Forms.Label IndexDirectoryPath;
         private System.Windows.Forms.Label CollectionDirectoryPath;
         private System.Windows.Forms.Button IndexCreate;
+        private System.Windows.Forms.Label label1;
     }
 }
 
